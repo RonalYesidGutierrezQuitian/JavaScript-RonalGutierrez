d92f492648e09b8cb4e53531037401ce3ff2f635
ronalGutierrez <152307741+RonalYesidGutierrezQuitian@users.noreply.github.com>
2024-04-12 07:54:14 -0500

    feat: :sparkles: Funcion conversion grados Celsius a Gracos Fahrenheit
    Se agrega 2 formulas para conversion grados Celsius a Gracos Fahrenheit

--- 1.FundamentosJavaScript/js/main.js
+++ 1.FundamentosJavaScript/js/main.js
@@ -1,2 +1,35 @@
-const name="Name"
-console.log("hola world")
+/*
+// usando la terminal de linux y con el comando node ruta-y-nombre-del-archivo.js-
+function celsiusToFahrenheit(celsius) {
+    var fahrenheit = 32 + (9 * celsius / 5);
+    return fahrenheit;
+}
+
+// Ejemplo de uso:
+var celsiusValue = 30;
+var fahrenheitValue = celsiusToFahrenheit(celsiusValue);
+console.log(celsiusValue + " grados Celsius son " + fahrenheitValue + " grados Fahrenheit.");
+
+ */
+
+    
+
+//Usando motor v8 de algùn navegador y solicitando al usuario la informacion de grados a convertir
+function celsiusToFahrenheit(celsius) {
+    var fahrenheit = 32 + (9 * celsius / 5);
+    return fahrenheit;
+}
+
+// Pedir al usuario que ingrese los grados Celsius
+var celsiusValue = prompt("Ingrese los grados Celsius a convertir a Fahrenheit:");
+
+// Convertir la entrada del usuario a un número
+celsiusValue = parseFloat(celsiusValue);
+
+// Verificar si la entrada es un número válido
+if (!isNaN(celsiusValue)) {
+    var fahrenheitValue = celsiusToFahrenheit(celsiusValue);
+    console.log(celsiusValue + " grados Celsius son " + fahrenheitValue + " grados Fahrenheit.");
+} else {
+    console.log("Por favor, ingrese un valor numérico válido para los grados Celsius.");
+}
